5b6bcfc5f7d4b62d5e3611f49d50932fd1ec0008
Ryan Laurie <iethree@gmail.com>
2023-09-12 08:41:42 +0100

    wip offline build script

--- release/run/release-offline.ts
+++ release/run/release-offline.ts
@@ -1,1 +1,24 @@
 // TODO: allow everything to still work if github is down and all we have is a local repo
+
+// get release version
+
+// get commit hash
+
+// check environment variables
+
+// check commit
+
+// build jar
+
+// test jar
+
+// tag releases
+
+// upload jar to s3
+
+// containerize and upload docker image
+
+// publish release notes
+
+// publish new version-info.json
+
